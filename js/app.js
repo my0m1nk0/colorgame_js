c092e6f241a1c445692cc200745d96dd5633b3d2
Micro Social <myomin@blue-stone.net>
2023-08-15 22:00:13 +0630

    continue function addd

--- js/app.js
+++ js/app.js
@@ -1,7 +1,6 @@
 const music = document.getElementById("play");
 const pause = document.getElementById("pause");
 const musicBg = document.getElementById("musicBG");
-const textContent = document.querySelector(".text-content")
 music.addEventListener('click',()=>{
     musicBg.play();
 
@@ -20,8 +19,8 @@ pause.addEventListener('click',()=>{
 
 //Play Btn
 document.addEventListener('DOMContentLoaded',()=>{
-const playBtn = document.getElementById("playGame");
-const loading = document.getElementById("loading");
+    const playBtn = document.getElementById("playGame");
+    const loading = document.getElementById("loading");
 
 
     playBtn.addEventListener('click', () => {
@@ -38,167 +37,4 @@ const loading = document.getElementById("loading");
         loading.classList.add('d-none');
     });
 });
-
-
-//Easy Play Game
-const easyBtn = document.getElementById("easy");
-const chooseLevel = document.getElementById("button-game");
-const showColor = document.getElementById('text-color');
-const colorRow = document.getElementById("colorRow");
-let colorBox=[];
-
-    easyBtn.addEventListener("click", ()=>{
-        easyGame();
-    });
-    function easyGame(){
-        chooseLevel.classList.add('d-none');
-        showColor.classList.remove('d-none');
-        createElement(3)
-    }
-
-//Normal Play Game
-const normalBtn = document.getElementById("normal");
-normalBtn.addEventListener("click", ()=>{
-    chooseLevel.classList.add('d-none');
-    showColor.classList.remove('d-none');
-    colorBox.push(createElement(6))
-});
-
-//Hard Play Game
-const hardBtn = document.getElementById("hard");
-hardBtn.addEventListener("click", ()=>{
-    chooseLevel.classList.add('d-none');
-    showColor.classList.remove('d-none');
-    colorBox.push(createElement(9))
-});
-
-//Back Home
-const backHome = document.getElementById("back");
-backHome.addEventListener('click',()=>{
-    window.location.href='index.html';
-})
-
-
-
-//Play Game
-// playBtn.addEventListener("click",()=>{
-//     colorBox =9;
-//     colors = generateColor(colorBox);
-//     randomPlayColor = colors[Math.random() *6];
-//     for (let i =0; i< colorBox.length; i++){
-//         colorBox[i].style.background = colors[i];
-//
-//     }
-//
-// })
-//Create Element
-function createElement(color){
-    let colorArry = generateColor(color);
-    console.log(colorArry);
-
-    let randomPlayColor = colorArry[Math.floor(Math.random() * color)];
-    console.log(randomPlayColor)
-    textContent.innerText=randomPlayColor;
-    for(let i =0; i<colorArry.length;i++){
-        const divBox = document.createElement('div');
-        divBox.classList.add('col-4')
-            divBox.style.background =colorArry[i];
-        divBox.addEventListener('click', function() {
-           let divBgColor=divBox.style.backgroundColor;
-
-            if(compareColors(randomPlayColor,divBgColor)){
-           document.addEventListener('DOMContentLoaded',()=>{
-               const modal = document.getElementById('modal');
-               const home = document.getElementById('home');
-               const continueGame = document.getElementById('continue');
-               showCongratulationsModal(modal);
-
-               continueGame.addEventListener('click',()=>{
-                   colorRow.innerHTML='';
-                   easyGame();
-                   hideCongratulationsModal(modal)
-
-
-               });
-               home.addEventListener('click',()=>{
-                   location.reload();
-               })
-
-           })
-            }else {
-                const wrongAns=document.getElementById("wrongAns");
-                wrongAns.classList.remove("d-none");
-
-                setTimeout(()=>{
-                    wrongAns.classList.add("d-none");
-                },1000)
-                
-               
-                divBox.style.background = '';
-            }
-        });
-
-        colorRow.appendChild(divBox);
-    }
-
-}
-// Function to show the modal
-function showCongratulationsModal(modal) {
-    let myModal = new bootstrap.Modal(modal);
-    myModal.show();
-}
-// Function to hide the modal
-function hideCongratulationsModal(modal) {
-    let myModal = new bootstrap.Modal(modal);
-    myModal.hide();
-}
-
-
-//Fail
-function failGame(a){
-
-//model box
-    //show Try Again
-}
-
-//Generate Color
-function generateColor(num){
-    let colorArray=[];
-    for(let i =0; i < num; i++){
-        colorArray.push(randomColor());
-    }
-    return colorArray;
-}
-
-//Random Color
-function randomColor(){
-    let red = Math.floor(Math.random() * 256);
-    let green = Math.floor(Math.random() * 256);
-    let blue = Math.floor(Math.random() * 256);
-    return `rgb(${red},${green},${blue})`;
-}
-
-//Compare Color
-function compareColors(color1, color2) {
-    if (!color1 || !color2) {
-        return false;
-    }
-
-    // Convert color strings to RGB components
-    let rgb1 = colorToRGB(color1);
-    let rgb2 = colorToRGB(color2);
-
-    // Compare RGB components
-    return rgb1.red === rgb2.red && rgb1.green === rgb2.green && rgb1.blue === rgb2.blue;
-}
-
-function colorToRGB(color) {
-    let rgbArray = color.match(/\d+/g);
-    console.log(rgbArray)
-    return {
-        red: parseInt(rgbArray[0]),
-        green: parseInt(rgbArray[1]),
-        blue: parseInt(rgbArray[2])
-    };
-}
 
---
+++ js/game.js
@@ -0,0 +1,146 @@
+
+
+//Easy Play Game
+const easyBtn = document.getElementById("easy");
+const chooseLevel = document.getElementById("button-game");
+const showColor = document.getElementById('text-color');
+const colorRow = document.getElementById("colorRow");
+const textContent = document.querySelector(".text-content");
+let colorBox=[];
+
+easyBtn.addEventListener("click", ()=>{
+easyGame();
+});
+function easyGame(){
+    chooseLevel.classList.add('d-none');
+    showColor.classList.remove('d-none');
+    createElement(3)
+}
+//Normal Play Game
+const normalBtn = document.getElementById("normal");
+normalBtn.addEventListener("click", ()=>{
+    chooseLevel.classList.add('d-none');
+    showColor.classList.remove('d-none');
+    colorBox.push(createElement(6))
+});
+
+//Hard Play Game
+const hardBtn = document.getElementById("hard");
+hardBtn.addEventListener("click", ()=>{
+    chooseLevel.classList.add('d-none');
+    showColor.classList.remove('d-none');
+    colorBox.push(createElement(9))
+});
+
+//Back Home
+const backHome = document.getElementById("back");
+backHome.addEventListener('click',()=>{
+    window.location.href='index.html';
+});
+
+
+//Create Element
+function createElement(color){
+    let colorArry = generateColor(color);
+    let randomPlayColor = colorArry[Math.floor(Math.random() * color)];
+    console.log(randomPlayColor)
+    textContent.innerText=randomPlayColor;
+    for(let i =0; i<colorArry.length;i++){
+        const divBox = document.createElement('div');
+        divBox.classList.add('col-4')
+        divBox.style.background =colorArry[i];
+        divBox.addEventListener('click', function() {
+            let divBgColor=divBox.style.backgroundColor;
+
+            if(compareColors(randomPlayColor,divBgColor)){
+                    // Your code here
+                const modal = document.getElementById('modal');
+                const home = document.getElementById('home');
+                const continueGame = document.getElementById('continue');
+                showCongratulationsModal(modal);
+
+                continueGame.addEventListener('click',()=>{
+                    colorRow.innerHTML='';
+                    easyGame();
+                    hideCongratulationsModal(modal);
+
+
+                });
+                home.addEventListener('click',()=>{
+                    location.reload();
+
+                });
+
+
+            }else {
+                const wrongAns=document.getElementById("wrongAns");
+                wrongAns.classList.remove("d-none");
+
+                setTimeout(()=>{
+                    wrongAns.classList.add("d-none");
+                },1000)
+
+
+                divBox.style.background = '';
+            }
+        });
+
+        colorRow.appendChild(divBox);
+    }
+
+}
+// Function to show the modal
+function showCongratulationsModal(modal) {
+    let myModal = new bootstrap.Modal(modal);
+    myModal.show();
+}
+// Function to hide the modal
+function hideCongratulationsModal(modal) {
+    modal.style.display = 'none'; // Hide the modal by setting its display style to 'none'
+    document.body.classList.remove('modal-open'); // Remove the 'modal-open' class from the body
+    const modalBackdrop = document.querySelector('.modal-backdrop');
+    if (modalBackdrop) {
+        modalBackdrop.remove(); // Remove the modal backdrop if it exists
+    }
+}
+
+//Generate Color
+function generateColor(num){
+    let colorArray=[];
+    for(let i =0; i < num; i++){
+        colorArray.push(randomColor());
+    }
+    return colorArray;
+}
+
+//Random Color
+function randomColor(){
+    let red = Math.floor(Math.random() * 256);
+    let green = Math.floor(Math.random() * 256);
+    let blue = Math.floor(Math.random() * 256);
+    return `rgb(${red},${green},${blue})`;
+}
+
+//Compare Color
+function compareColors(color1, color2) {
+    if (!color1 || !color2) {
+        return false;
+    }
+
+    // Convert color strings to RGB components
+    let rgb1 = colorToRGB(color1);
+    let rgb2 = colorToRGB(color2);
+
+    // Compare RGB components
+    return rgb1.red === rgb2.red && rgb1.green === rgb2.green && rgb1.blue === rgb2.blue;
+}
+
+function colorToRGB(color) {
+    let rgbArray = color.match(/\d+/g);
+    console.log(rgbArray)
+    return {
+        red: parseInt(rgbArray[0]),
+        green: parseInt(rgbArray[1]),
+        blue: parseInt(rgbArray[2])
+    };
+}
